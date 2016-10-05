7a51882e48f1da0308bdee7fb7b953441e871eee
manolis9 <manolis@alumni.ubc.ca>
2016-10-05 16:13:35 -0700

    Update todos functionality

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post('/todos', function(req, res) {
 	res.json(body);
 });
 
-//DELETE /todos/:id
+// DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
@@ -67,6 +67,37 @@ app.delete('/todos/:id', function(req, res) {
 	} else {	
 		res.status(404).json({"Error": "no todo found with that id"}); //not found
 	}
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	// Allow todos to have only description and completed fields
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
 });
 
 app.listen(PORT, function() {
