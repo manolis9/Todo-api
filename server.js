926506855e476a61b222761416b76a861ee0723b
manolis9 <manolis@alumni.ubc.ca>
2016-10-06 10:05:01 -0700

    Practicing with sequelize

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ var todos = [];
 var todoNextId = 1;
 
 /* whenever a json request comes in, express
-*  parses it and we can access it via req.body.
-*/
-app.use(bodyParser.json()); 
+ *  parses it and we can access it via req.body.
+ */
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
@@ -21,13 +21,17 @@ app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
 
-	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
-		filteredTodos = _.where(filteredTodos, {completed: true});
-	} else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false});
-	} 
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {
+			completed: true
+		});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {
+			completed: false
+		});
+	}
 
-	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
 		filteredTodos = _.filter(filteredTodos, function(todo) {
 			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
 		});
@@ -39,14 +43,16 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 
-	if(matchedTodo){
+	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send(); //not found
 	}
-		
+
 });
 
 // POST /todos
@@ -55,7 +61,7 @@ app.post('/todos', function(req, res) {
 	// Allow todos to have only description and completed fields
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send(); //request can't be completed because bad data was provided (Bad Request)
 	}
 
@@ -74,13 +80,17 @@ app.post('/todos', function(req, res) {
 app.delete('/todos/:id', function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 
-	if(matchedTodo){
+	if (matchedTodo) {
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
-	} else {	
-		res.status(404).json({"Error": "no todo found with that id"}); //not found
+	} else {
+		res.status(404).json({
+			"Error": "no todo found with that id"
+		}); //not found
 	}
 });
 
@@ -88,25 +98,27 @@ app.delete('/todos/:id', function(req, res) {
 app.put('/todos/:id', function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 
 	// Allow todos to have only description and completed fields
 	var body = _.pick(req.body, 'description', 'completed');
 	var validAttributes = {};
 
-	if(!matchedTodo){
+	if (!matchedTodo) {
 		return res.status(404).send();
 	}
 
-	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
 		validAttributes.completed = body.completed;
-	} else if(body.hasOwnProperty('completed')){
+	} else if (body.hasOwnProperty('completed')) {
 		return res.status(400).send();
 	}
 
-	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
 		validAttributes.description = body.description;
-	} else if(body.hasOwnProperty('description')){
+	} else if (body.hasOwnProperty('description')) {
 		return res.status(400).send();
 	}
 
