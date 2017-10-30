7bdcdb6a47403606eefd7ae1a0ac5b3fed95594a
manolis9 <manioannides@gmail.com>
2017-10-29 20:39:50 -0700

    Add login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('user', {
+    var user = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -35,9 +35,32 @@ module.exports = function (sequelize, DataTypes) {
     }, {
             hooks: {
                 beforeValidate: function (user, options) {
-                    if(typeof user.email === 'string') {
-                       user.email = user.email.toLowerCase();
+                    if (typeof user.email === 'string') {
+                        user.email = user.email.toLowerCase();
                     }
+                }
+            },
+            classMethods: {
+                authenticate: function (body) {
+                    return new Promise(function (resolve, reject) {
+                        if ((typeof body.email !== 'string') || (typeof body.password !== 'string')) {
+                            return reject();
+                        }
+
+                        user.findOne({
+                            where: {
+                                email: body.email
+                            }
+                        }).then(function (user) {
+                            if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+                                return reject();
+                            }
+                            resolve(user);
+
+                        }, function (e) {
+                            reject();
+                        });
+                    });
                 }
             },
             instanceMethods: {
@@ -47,4 +70,6 @@ module.exports = function (sequelize, DataTypes) {
                 }
             }
         });
+
+        return user;
 };
